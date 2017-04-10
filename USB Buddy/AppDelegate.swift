4c3403e80ad0e87a3a2a91b954ec76433dfb834b
Kevin Alcock <kalcock@dynamiccontrols.com>
2017-04-10 19:06:27 +1200

    Initial Commit

---
+++ USB Buddy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  USB Buddy
+//
+//  Created by Kevin Alcock on 10/04/17.
+//  Copyright © 2017 Katipo Information Security Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ USB Buddy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  USB Buddy
+//
+//  Created by Kevin Alcock on 10/04/17.
+//  Copyright © 2017 Katipo Information Security Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
